b33cb8f90672ce0498bba370375d9c81734b0a31
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2024-04-28 13:28:21 +0100

    add cryptland

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,11 @@
 function Project(props) {
   return (
-    <div className="rounded-2xl flex flex-col justify-center items-center bg-white h-fit sm:p-2 sm:m-5 sm:w-96 w-10/12 max-w-80w font-serif mt-3">
+    <div className="rounded-2xl flex flex-col justify-center items-center bg-white max-h-fit sm:p-2 sm:m-5 sm:w-96 w-10/12 max-w-80w font-serif mt-3">
       <div className="h-auto">
         <img
           src={props.img}
           alt=""
-          className=" rounded-xl object-contain h-64 w-11/12 m-auto"
+          className=" rounded-xl object-contain max-h-64 w-11/12 m-auto"
         />
       </div>
       <div className="flex flex-col justify-center items-center p-6 pt-1">
@@ -35,9 +35,17 @@ function Project(props) {
 
 const projects = [
   {
+    img: "./images/cryptland.png",
+    name: "Cryptland",
+    logo: ["./images/react.svg", "./images/tailwind.svg"],
+    description: "The Finance Solutions For Your Business",
+    github: "https://github.com/Abdulmajid48/cryptland",
+    server: "https://cryptland.vercel.app/",
+  },
+  {
     img: "./images/tripace.png",
     name: "Tripace",
-    logo: ["./images/bootstrap.png", "./images/react.png"],
+    logo: ["./images/bootstrap.svg", "./images/react.svg"],
     description:
       " A 17 pages Flight booking platform with taxi services and hotel accommodations.",
     github: "https://github.com/Abdulmajid48/tripace",
@@ -47,7 +55,7 @@ const projects = [
   {
     img: "./images/resultsummary.png",
     name: "Results summary",
-    logo: ["./images/html.png", "./images/css.png"],
+    logo: ["./images/html.svg", "./images/css.svg"],
     description:
       "A solution to the Results summary component challenge on Frontend Mentor.",
     github: "https://github.com/Abdulmajid48/resultsummary",
@@ -56,7 +64,7 @@ const projects = [
   {
     img: "./images/todolistapp.png",
     name: "Todo App",
-    logo: ["./images/react.png"],
+    logo: ["./images/react.svg"],
     description: "A solution to the Todo App challenge on Frontend Mentor.",
     github: "https://github.com/Abdulmajid48/todolistapp",
     server: "https://todolistapp-snkz-g9qo2rpg0-abdulmajid48.vercel.app/9",
@@ -65,7 +73,7 @@ const projects = [
   {
     img: "./images/testimonials.png",
     name: "Testimonials",
-    logo: ["./images/html.png", "./images/css.png"],
+    logo: ["./images/html.svg", "./images/css.svg"],
     description:
       "A solution to the Testimonials Grid Section on Frontend Mentor.",
     github: "https://github.com/Abdulmajid48/testimonials",
@@ -74,7 +82,7 @@ const projects = [
   {
     img: "./images/todolist.png",
     name: "Todolist",
-    logo: ["./images/ejs.svg", "./images/postgresql.png"],
+    logo: ["./images/ejs.svg", "./images/postgresql.svg"],
     description: "A Todolist built with Postgressql and Ejs.",
     github: "https://github.com/Abdulmajid48/todolist",
     server: "",
@@ -82,7 +90,7 @@ const projects = [
   {
     img: "./images/drumkit.png",
     name: "Drumkit",
-    logo: ["./images/html.png", "./images/css.png", "./images/javascript.png"],
+    logo: ["./images/html.svg", "./images/css.svg", "./images/javascript.svg"],
     description: "A Drumkit built with html, css and javascript.",
     github: "https://github.com/Abdulmajid48/drumkit",
     server: "https://drumkit-ecru-one.vercel.app/",
@@ -90,7 +98,7 @@ const projects = [
   {
     img: "./images/mondrian.png",
     name: "Mondrain Art",
-    logo: ["./images/html.png", "./images/css.png"],
+    logo: ["./images/html.svg", "./images/css.svg"],
     description: "A Mondrian Art built with html and css Grid",
     github: "https://github.com/Abdulmajid48/mondrian",
     server: "https://mondrian-ivory.vercel.app/",
@@ -98,7 +106,7 @@ const projects = [
   {
     img: "./images/tindog.png",
     name: "Tindog",
-    logo: ["./images/html.png", "./images/css.png", "./images/bootstrap.png"],
+    logo: ["./images/html.svg", "./images/css.svg", "./images/bootstrap.svg"],
     description: "A Drumkit built with html, css and bootstrap.",
     github: "https://github.com/Abdulmajid48/tindogwebsite",
     server: "https://tindogwebsite.vercel.app//",
@@ -106,7 +114,7 @@ const projects = [
   {
     img: "./images/simongame.png",
     name: "Simon Game Challenge",
-    logo: ["./images/html.png", "./images/css.png", "./images/javascript.png"],
+    logo: ["./images/html.svg", "./images/css.svg", "./images/javascript.svg"],
     description: "A Simon game built with html, css and javascript",
     github: "https://github.com/Abdulmajid48/simongamechallenge",
     server: "https://simongamechallenge.vercel.app/",
@@ -114,7 +122,7 @@ const projects = [
   {
     img: "./images/portfoliosample.png",
     name: "Portfolio Sample",
-    logo: ["./images/html.png", "./images/css.png"],
+    logo: ["./images/html.svg", "./images/css.svg"],
     description: "A Portfolio website built with html and css",
     github: "https://github.com/Abdulmajid48/portfoliosample",
     server: "https://portfoliosample-theta.vercel.app/",
@@ -122,7 +130,7 @@ const projects = [
   {
     img: "./images/memeproject.png",
     name: "Meme Project",
-    logo: ["./images/html.png", "./images/css.png"],
+    logo: ["./images/html.svg", "./images/css.svg"],
     description: "A Portfolio website built with html and css",
     github: "https://github.com/Abdulmajid48/memeproject",
     server: "https://memeproject-ten.vercel.app/",
