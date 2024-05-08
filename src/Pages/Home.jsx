469f4f9e1fe99161e419f8a872921fe32d6beb5a
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2024-05-08 17:07:41 +0100

    add resume

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,70 +1,7 @@
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import XIcon from "@mui/icons-material/X";
-import Navbar from "../components/Navbar";
-import { NavLink } from "react-router-dom";
+import Homepage from "../components/Homepage";
 
 function Home(props) {
-  return (
-    <div>
-      <Navbar />
-      <div className="bg-img min-w-fit min-h-screen bg-cover bg-fifty flex flex-row justify-center font-serif">
-        <div className="flex flex-col justify-center items-center py-8 text-rose-50 text-3xl gap-10 text-center">
-          <div className="">
-            <img
-              src="./images/profile.jpg"
-              alt=""
-              className="rounded-full h-40 w-40 object-cover"
-            />
-          </div>
-          <div className="text-3xl sm:w-full w-11/12">
-            Abdul-Majid Aladejana
-          </div>
-          <div className="text-3xl w-11/12">Web developer</div>
-          <div className="flex flex-row justify-between text-rose-50 gap-3">
-            <div className="">
-              <NavLink to="https://github.com/Abdulmajid48">
-                {" "}
-                <GitHubIcon sx={{ fontSize: 40 }} style={{ color: "white" }} />
-              </NavLink>{" "}
-            </div>
-            <div>
-              <NavLink to="https://www.linkedin.com/in/abdul-majid-aladejana/">
-                {" "}
-                <LinkedInIcon
-                  sx={{ fontSize: 40 }}
-                  style={{ color: "white" }}
-                />
-              </NavLink>
-            </div>
-            <div>
-              <NavLink to="https://www.x.com/aladejanaamajid">
-                {" "}
-                <XIcon sx={{ fontSize: 40 }} style={{ color: "white" }} />
-              </NavLink>
-            </div>
-          </div>
-          <div className="h-12 w-40 rounded-lg border-solid border-2 border-white hover:bg-white hover:text-black p-1 flex flex-row items-center justify-around">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
-              />
-            </svg>
-            <div> Résumé</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Homepage />;
 }
 
 export default Home;
